983fc2489fdbd9a9fdf22e72b507d7e854b3f940
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-03-21 22:45:31 +0100

    feat: add react slider add a lot of useStates
    - responsivity for Gamemap
    - styling for slider
    - lot of stuff in home.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,24 @@ import Head from 'next/head'
 import GameMap from '@/components/GameMap'
 import React, { useState } from 'react';
 import TileSelector from '@/components/TileSelector';
+import Archive from '@/components/Archive';
+import { initialMap, BASE_URL } from './constants';
+import RangeSlider from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
 
 
-export default function Home() {
+
+export default function Home(data) {
   const [selectedTileType, setSelectedTileType] = useState(1);
-  const [map, setMap] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
-    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 6, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 5, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 1, 6, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 3, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 2, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
+  const [map, setMap] = useState(initialMap);
+  const [symmetry, setSymmetry] = useState(10);
+  const [pathLength, setPathLength] = useState(10);
+  const [sliderValue, setSliderValue] = useState("0,0");
+
+
+  const symmetries = data.data.behaviours[0] 
+  const paths = data.data.behaviours[1]
+  const objectives = data.data.behaviours[2] 
 
   const handleMapChange = (newMap) => {
     setMap(newMap);
@@ -33,13 +29,15 @@ export default function Home() {
     setSelectedTileType(tileType);
   };
 
-  const handleClick = () => {
-    console.log(map);
+  const handlePointClick = (selectedSymmetry, selectedPathLength) => {
+    setSymmetry(selectedSymmetry);
+    setPathLength(selectedPathLength);
   };
 
-  //post request to api which passes the map data and gets back a new generated map
+
   async function generateMap() {
-    await fetch('https://nca-backend-rxv2teft2q-ew.a.run.app/generate?path_length=35&symmetry=0.4', {
+    document.documentElement.style.cursor = "wait"
+    await fetch(`${BASE_URL}/generate?path_length=${pathLength}&symmetry=${symmetry}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -53,19 +51,35 @@ export default function Home() {
       .catch((error) => {
         console.error('Error:', error);
       });
+      document.documentElement.style.cursor = "default"
   }
-
-
   return (
-    <div className="py-5 flex items-center justify-center">
+    <div className="flex flex-col md:flex-row w-full h-screen">
       <Head>
         <title>Interactive NCA</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="./zelda.png" />
       </Head>
-      <main className="flex justify-center w-full text-center">
+      <div className="w-full md:w-1/2 h-full flex items-center justify-center">
         <div className="container mx-auto flex flex-col items-center justify-center">
-          <h1 className="text-3xl font-press-start">
+          <h1 className="text-xl sm:text-3xl font-press-start pb-20">
+            Models 
+          </h1>
+        <Archive
+        symmetries={symmetries}
+        paths={paths}
+        objectives={objectives}
+        handlePointClick={handlePointClick}
+        currSymmetry={symmetry}
+        currPath={pathLength}
+        />
+        <p className= "font-press-start pt-10" >Symmetry: <span className='text-sky-400'>{symmetry}</span></p>
+        <p className= "font-press-start" >Path: <span className='text-sky-400'>{pathLength}</span></p> 
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 h-full py-5 flex items-center justify-center">
+        <div className="container mx-auto flex flex-col items-center justify-center">
+          <h1 className="text-xl sm:text-3xl font-press-start">
             Interactive NCA
           </h1>
           <div className="py-5">
@@ -79,11 +93,32 @@ export default function Home() {
           selectedTileType={selectedTileType}
           onTileTypeSelect={handleTileTypeSelect}
           />
-          <button className="bg-green-700 hover:bg-green-900 font-press-start text-white font-bold mt-10 py-2 px-4 full" onClick={generateMap}>
+          <button className="bg-green-700 hover:bg-green-900 font-press-start text-white font-bold mt-2 py-2 px-4 full" onClick={generateMap}>
             Generate
           </button>
+
+          <div className="flex pt-10 w-1/2 flex-col items-center justify-center">
+          <RangeSlider id="range-slider-yellow" 
+            defaultValue={[0, 0]}
+            onInput={setSliderValue}
+            max={50}
+            min={0}
+            step={1}
+            thumbsDisabled={[true, false]}
+            rangeSlideDisabled={true} 
+          />
+            </div>
+            <h2>{ parseInt(String(sliderValue).split(",")[1]) } </h2>
         </div>
-      </main>
+      </div>
     </div>
   )
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+  const response = await fetch(`${BASE_URL}/behaviours`);
+  const data = await response.json();
+  
+  return { props: { data } };
 }
