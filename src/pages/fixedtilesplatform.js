3e14973190a9a75a3a8ed58273e8d8511c7e17f7
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-03-28 14:31:38 +0200

    feat: save fixed maps

--- src/pages/fixedtilesplatform.js
+++ src/pages/fixedtilesplatform.js
@@ -2,10 +2,9 @@ import Head from 'next/head'
 import React, { useState } from 'react';
 import 'react-range-slider-input/dist/style.css';
 
-import { initialMap, BASE_URL } from '@/utils/constants';
+import { initialMap } from '@/utils/constants';
 import TileSelector from '@/components/TileSelector';
 import GameMap from '@/components/GameMap'
-import next from 'next';
 
 export default function FixedTilesPlatform() {
   const [selectedTileType, setSelectedTileType] = useState(1);
@@ -21,12 +20,32 @@ export default function FixedTilesPlatform() {
   };
 
   const nextMap = () => {
-    setFixedTilesMaps([...fixedTilesMaps, map]);
+    const copyMap = map.map((row) => [...row]); // create a new copy of each row array
+    setFixedTilesMaps([...fixedTilesMaps, copyMap]);
+    console.log(fixedTilesMaps);
   }
 
-  const saveMaps = () => {
-    return
-  }
+  function saveMaps() {
+  // Convert the tileMaps 3d array to a typed array
+  const typedArray = new Int32Array(fixedTilesMaps.flat(2));
+  
+  // Create a Blob object from the typed array
+  const blob = new Blob([typedArray], { type: 'application/x-binary' });
+
+  // Create a link element and set its href attribute to the Blob object's URL
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  
+  // Set the link element's download attribute to the desired filename
+  link.download = 'tile_maps.npy';
+  
+  // Append the link element to the document body and trigger a click event
+  document.body.appendChild(link);
+  link.click();
+  
+  // Remove the link element from the document body
+  document.body.removeChild(link);
+}
   
   return (
     <div className="flex flex-col md:flex-row w-full h-screen">
@@ -52,6 +71,7 @@ export default function FixedTilesPlatform() {
           onTileTypeSelect={handleTileTypeSelect}
           />
 
+          <h2 className='py-5 font-press-start'>Current number of maps with fixed tiles: {fixedTilesMaps.length}</h2>
           <div className='flex px-5'>
             <button className="bg-green-700 hover:bg-green-900 font-press-start text-white font-bold mt-2 mr-2 py-2 px-4 full" onClick={nextMap}>
               Next
