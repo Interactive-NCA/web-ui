bdefdf6bed84d3c792a61a5c789a788c4f61510b
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-03-29 15:08:26 +0200

    feat: add binary map, refactoring of code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,46 @@
+// External imports
 import Head from 'next/head'
 import React, { useState } from 'react';
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 
-import { initialMap, BASE_URL } from '@/utils/constants';
+// Helper things
+import { BASE_URL } from '@/utils/constants';
+import { generateEmptyMap } from '@/utils/generateEmptyMap';
+
+// Components
 import TileSelector from '@/components/TileSelector';
 import Archive from '@/components/Archive';
 import GameMap from '@/components/GameMap'
 
 export default function Home(data) {
+  // Map states
   const [selectedTileType, setSelectedTileType] = useState(1);
-  const [map, setMap] = useState(initialMap);
+  const [map, setMap] = useState(generateEmptyMap());
+  const [binary, setBinary] = useState(generateEmptyMap());
+  const [steps, setSteps] = useState(null);
+  const [mapGenerated, setMapGenerated] = useState(false);
+  // Behaviours states
   const [symmetry, setSymmetry] = useState(10);
   const [pathLength, setPathLength] = useState(10);
+  // Slider states
   const [sliderValue, setSliderValue] = useState(0);
-  const [steps, setSteps] = useState(null);
   const [sliderKey, setSliderKey] = useState(0);
   const [disabledSlider, setDisableSlider] = useState(false);
   const [sliderDefault, setSliderDefault] = useState(0);
-
+  
+  // Get behaviours from backend
   const symmetries = data.data.behaviours[0] 
   const paths = data.data.behaviours[1]
   const objectives = data.data.behaviours[2] 
 
+  // Handlers that are passed to individual components
   const handleMapChange = (newMap) => {
     setMap(newMap);
+  };
+
+  const handleBinaryChange = (newBinary) => {
+    setBinary(newBinary);
   };
 
   const handleTileTypeSelect = (tileType) => {
@@ -39,43 +55,57 @@ export default function Home(data) {
   const handleSliderChange = (value) => {
     const parsedValue = parseInt(String(value).split(",")[1])
     setSliderValue(parsedValue)
-    if (steps == null){
-      alert("Please generate a map first");
-    } else {
-      setMap(steps[parsedValue])
+    if (!mapGenerated){
+      alert("Please generate a map first!");
+      setMapGenerated(true)
+    } 
+    else if (steps != null) {
+        setMap(steps[parsedValue])
     }
   }
 
-  function resetSlider(value) {
+  // Re-render slider
+  const resetSlider = (value) => {
     setSliderDefault(value);
     setSliderValue(value)
     setSliderKey(sliderKey + 1);
   }
-function activateAnimate() {
-  if (steps == null) {
-    alert("Please generate a map first");
-  } else {
 
-    setDisableSlider(true);
-    var counter = 0;
-    for (let i = 0; i < steps.length; i++) {
-      setTimeout(() => {
-        setSliderValue(i);
-        setMap(steps[i]);
-        counter = counter + 1;
+  // Button to reset everything
+  const resetMaps = () => {
+    setMap(generateEmptyMap());
+    setBinary(generateEmptyMap());
+    setSteps(null);
+    setMapGenerated(false);
+    resetSlider(0);
+  }
 
-        if (counter == steps.length) {
-          setTimeout(() => {
-            resetSlider(49);
-            setDisableSlider(false);
-          }, 100);
-        }
-      }, 100 * i);
+  // Button to animate the generation process
+  const activateAnimate = () => {
+    if (steps == null) {
+      alert("Please generate a map first");
+    } else {
+
+      setDisableSlider(true);
+      var counter = 0;
+      for (let i = 0; i < steps.length; i++) {
+        setTimeout(() => {
+          setSliderValue(i);
+          setMap(steps[i]);
+          counter = counter + 1;
+
+          if (counter == steps.length) {
+            setTimeout(() => {
+              resetSlider(steps.length-1);
+              setDisableSlider(false);
+            }, 100);
+          }
+        }, 100 * i);
+      }
     }
   }
-}
 
-  
+  // Main function to generate the map (calls backend)
   async function generateMap() {
     document.documentElement.style.cursor = "wait"
     await fetch(`${BASE_URL}/generate?path_length=${pathLength}&symmetry=${symmetry}`, {
@@ -90,6 +120,7 @@ function activateAnimate() {
         setMap(data.generated_map[0])
         setSteps(data.generated_map)
         resetSlider(0)
+        setMapGenerated(true)
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -108,16 +139,16 @@ function activateAnimate() {
           <h1 className="text-xl sm:text-3xl font-press-start pb-20">
             Models 
           </h1>
-        <Archive
-        symmetries={symmetries}
-        paths={paths}
-        objectives={objectives}
-        handlePointClick={handlePointClick}
-        currSymmetry={symmetry}
-        currPath={pathLength}
-        />
-        <p className= "font-press-start pt-10" >Symmetry: <span className='text-sky-400'>{symmetry}</span></p>
-        <p className= "font-press-start" >Path: <span className='text-sky-400'>{pathLength}</span></p> 
+          <Archive
+          symmetries={symmetries}
+          paths={paths}
+          objectives={objectives}
+          handlePointClick={handlePointClick}
+          currSymmetry={symmetry}
+          currPath={pathLength}
+          />
+          <p className= "font-press-start pt-10" >Symmetry: <span className='text-sky-400'>{symmetry}</span></p>
+          <p className= "font-press-start" >Path: <span className='text-sky-400'>{pathLength}</span></p> 
         </div>
       </div>
       <div className="w-full md:w-1/2 h-full py-5 flex items-center justify-center">
@@ -128,7 +159,9 @@ function activateAnimate() {
           <div className="py-5">
             <GameMap 
             mapData={map} 
+            binaryData={binary}
             onMapChange={handleMapChange} 
+            onBinaryChange={handleBinaryChange}
             selectedTileType={selectedTileType} 
             />
           </div>
@@ -143,6 +176,11 @@ function activateAnimate() {
             </button>
             <button className="bg-green-700 hover:bg-green-900 font-press-start text-white font-bold mt-2 py-2 px-4 full" onClick={activateAnimate}>
               Animate 
+            </button>
+          </div>
+          <div className='flex'>
+            <button className="bg-green-700 hover:bg-green-900 font-press-start text-white font-bold mt-2 py-2 px-4 full" onClick={resetMaps}>
+              Reset 
             </button>
           </div>
 
