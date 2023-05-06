fa9f82a4bd6d57e681962602e2af758464f3f54a
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-05-06 16:18:24 +0200

    style(MiniMapGrid.js): remove md prefix from grid-cols and grid-rows classes style(MiniMap.module.css): remove media query for smaller screens

--- src/components/MiniMapGrid.js
+++ src/components/MiniMapGrid.js
@@ -2,7 +2,7 @@ import MiniMap from '@/components/MiniMap';
 
 function MiniMapGrid({ trainingSeeds, binaryMap, handleMiniMapSelect }) {
   return (
-    <div className='grid gap-4 p-4 md:grid-cols-5 md:grid-rows-2'>
+    <div className='grid gap-4 p-4 grid-cols-5 grid-rows-2'>
       {trainingSeeds.map((seed, index) => (
         <MiniMap key={index} mapData={seed} binaryMap={binaryMap[index]} handleMiniMapSelect={handleMiniMapSelect} />
       ))}
