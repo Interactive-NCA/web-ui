b307b24a8a858777c2044c6dc03d11d03b312644
Lukas Rasocha <rasocha.lukas@gmail.com>
2024-01-03 19:18:52 +0100

    remove google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,41 +1,9 @@
-import '@/styles/globals.css'
-import Script from "next/script"
-import { useRouter } from 'next/router';
-import { useEffect } from "react";
-import * as gtag from '@/utils/gtag'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
- 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
- 
-    router.events.on("routeChangeComplete", handleRouteChange);
- 
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-  return (
-    <>
-    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-N0PMSYD4PB"></Script>
-    <Script
-      id='google-analytics'
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-N0PMSYD4PB', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-    />
-  <Component {...pageProps} />
-  </>
-  )
+	return (
+		<>
+			<Component {...pageProps} />
+		</>
+	);
 }
